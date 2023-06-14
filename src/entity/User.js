108d2881870382af017f696f6a961f70f5219098
IdirX69 <adil.idirr@gmail.com>
2023-06-14 16:01:49 +0200

    start

--- src/entity/User.js
+++ src/entity/User.js
@@ -1,97 +1,105 @@
 class User {
-    #id;
-    #first_name = "";
-    #last_name = "";
-    #birthdate = "";
-    #email = "";
-    #password = "";
-    #address = "";
-    #username = "";
+  #id;
+  #first_name = "";
+  #last_name = "";
+  #birthdate = "";
+  #email = "";
+  #password = "";
+  #address = "";
+  #username = "";
 
-    //getters
-    get id() {
-        return this.#id;
-    }
+  //getters
+  get id() {
+    return this.#id;
+  }
 
-    get first_name() {
-        return this.#first_name;
-    }
+  get first_name() {
+    return this.#first_name;
+  }
 
-    get last_name() {
-        return this.#last_name;
-    }
+  get last_name() {
+    return this.#last_name;
+  }
 
-    get birthdate() {
-        return this.#birthdate;
-    }
+  get birthdate() {
+    return this.#birthdate;
+  }
 
-    get email() {
-        return this.#email;
-    }
+  get email() {
+    return this.#email;
+  }
 
-    get password() {
-        return this.#password;
-    }
+  get password() {
+    return this.#password;
+  }
 
-    get address() {
-        return this.#address;
-    }
+  get address() {
+    return this.#address;
+  }
 
-    get username() {
-        return this.#username;
-    }
+  get username() {
+    return this.#username;
+  }
 
-    toJSON() {
-        return {
-            id: this.#id,
-            first_name: this.#first_name,
-            last_name: this.#last_name,
-            birthdate: this.#birthdate,
-            email: this.#email,
-            address: this.#address,
-            username: this.#username
-        }
-    }
+  toJSON() {
+    return {
+      id: this.#id,
+      first_name: this.#first_name,
+      last_name: this.#last_name,
+      birthdate: this.#birthdate,
+      email: this.#email,
+      address: this.#address,
+      username: this.#username,
+    };
+  }
 
-    getColumns() {
-        return ['first_name', 'last_name', 'birthdate', 'email', 'address', 'username', 'password'];
-    }
+  getColumns() {
+    return [
+      "first_name",
+      "last_name",
+      "birthdate",
+      "email",
+      "address",
+      "username",
+      "password",
+    ];
+  }
 
-    //setters
-    set id(id) {
-        this.#id = id;
-    }
+  //setters
+  set id(id) {
+    this.#id = id;
+  }
 
-    set first_name(first_name) {
-        if (first_name === '') {
-            throw new Error(`first_name field of User cannot be empty`);
-        }
-        this.#first_name = first_name;
+  set first_name(first_name) {
+    if (first_name === "") {
+      throw new Error(`first_name field of User cannot be empty`);
     }
+    this.#first_name = first_name;
+  }
 
-    set last_name(last_name) {
-        this.#last_name = last_name;
-    }
+  set last_name(last_name) {
+    this.#last_name = last_name;
+  }
 
-    set birthdate(birthdate) {
-        this.#birthdate = birthdate;
-    }
+  set birthdate(birthdate) {
+    this.#birthdate = birthdate;
+  }
 
-    set email(email) {
-        this.#email = email;
-    }
+  set email(email) {
+    this.#email = email;
+  }
 
-    set password(password) {
-        this.#password = password;
-    }
+  set password(password) {
+    this.#password = password;
+  }
 
-    set address(address) {
-        this.#address = address;
-    }
+  set address(address) {
+    this.#address = address;
+  }
 
-    set username(username) {
-        this.#username = username;
-    }
+  set username(username) {
+    this.#username = username;
+  }
 }
 
 module.exports = User;
--- src/model/orm/EntityManager.js
+++ src/model/orm/EntityManager.js
@@ -1,12 +1,26 @@
-const AbstractEntityManager = require('./AbstractEntityManager');
+const AbstractEntityManager = require("./AbstractEntityManager");
 
 class EntityManager extends AbstractEntityManager {
-    insert(entity, dataEntity, tableName) {
-        let entityToInsert = super.EntityGenerator(entity, dataEntity);
-        let {sql, values} = super.SqlQueryGenerator(tableName, entityToInsert, 'insert');
+  insert(entity, dataEntity, tableName) {
+    let entityToInsert = super.EntityGenerator(entity, dataEntity);
+    let { sql, values } = super.SqlQueryGenerator(
+      tableName,
+      entityToInsert,
+      "insert"
+    );
 
-        return super.ExecuteInsert(sql, values, entity, tableName)
-    }
+    return super.ExecuteInsert(sql, values, entity, tableName);
+  }
+  update(entity, dataEntity, tableName, id) {
+    let entityToInsert = super.EntityGenerator(entity, dataEntity);
+    let { sql, values } = super.SqlQueryGenerator(
+      tableName,
+      entityToInsert,
+      "update"
+    );
+
+    return super.ExecuteInsert(sql, values, entity, tableName);
+  }
 }
 
 module.exports = EntityManager;
